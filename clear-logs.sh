1c20a7fab252aeedf97b27c6cfb115774633c313
vadim <105228689+guessiwillbefine@users.noreply.github.com>
2023-03-06 11:36:53 +0200

    added root log directory to bash script that clear log directories

--- clear-logs.sh
+++ clear-logs.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
-rm -r ./test/*
-echo done!
+echo 'clear root log folder?'
+echo '(Y\N):'
+read -r answer
+if [ "$answer" = 'Y' ]; then rm -r ./logs/*
+else echo 'skipped'
+fi
+
+echo 'clear ms-api log folder?'
+echo '(Y\N):'
+read -r answer2
+if [ "$answer2" = 'Y' ]; then rm -r ./ms-api/logs/*
+else echo 'skipped'
+fi
+echo 'done!'
