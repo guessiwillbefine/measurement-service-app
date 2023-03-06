38554fd092748b64179ef6f56fb960cd6a5e27cd
vadim <105228689+guessiwillbefine@users.noreply.github.com>
2023-03-06 11:17:27 +0200

    set up rabbit, created single queue for test connection

---
+++ ms-api/src/main/java/ua/ms/configuration/rabbit/RabbitMQConfiguration.java
@@ -0,0 +1,34 @@
+package ua.ms.configuration.rabbit;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfiguration {
+    @Value("${queue.name}")
+    private String queue;
+    @Value("${queue.exchange}")
+    private String exchange;
+    @Value("${queue.routing-key}")
+    private String routingKey;
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(exchange);
+    }
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queue);
+    }
+
+    @Bean
+    public Binding binding(DirectExchange exchange, Queue queue) {
+        return BindingBuilder.bind(queue)
+                .to(exchange).with(routingKey);
+    }
+}
