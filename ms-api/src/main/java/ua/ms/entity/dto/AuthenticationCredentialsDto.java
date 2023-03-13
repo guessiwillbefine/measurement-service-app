27bfe747b2edc7b137e6eba854fae9f87bb6955f
vadim <105228689+guessiwillbefine@users.noreply.github.com>
2023-03-13 22:23:57 +0200

    done machine entity

--- ms-api/src/main/java/ua/ms/entity/dto/AuthenticationCredentialsDto.java
+++ ms-api/src/main/java/ua/ms/entity/dto/AuthenticationCredentialsDto.java
@@ -1,6 +1,5 @@
 package ua.ms.entity.dto;
 
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.extern.jackson.Jacksonized;
@@ -10,7 +9,6 @@ import ua.ms.util.ApplicationConstants;
 @Getter
 @Builder
 @Jacksonized
-@Tag(name = "Authentication DTO")
 public class AuthenticationCredentialsDto {
     @Length(min = ApplicationConstants.Validation.MIN_USERNAME_LENGTH,
             max = ApplicationConstants.Validation.MAX_USERNAME_LENGTH,
@@ -18,6 +16,6 @@ public class AuthenticationCredentialsDto {
     private String username;
     @Length(min = ApplicationConstants.Validation.MIN_USERNAME_LENGTH,
             max = ApplicationConstants.Validation.MAX_USERNAME_LENGTH,
-            message = ApplicationConstants.Validation.USERNAME_MSG)
+            message = ApplicationConstants.Validation.PASSWORD_MSG)
     private String password;
 }
