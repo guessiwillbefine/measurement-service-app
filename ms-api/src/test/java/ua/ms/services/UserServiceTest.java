9cc5a9b92e7a3b15af54ee8b29049a8368e745dc
Moksem <maks.viskovatov@gmail.com>
2023-03-22 03:21:25 +0200

    Some changes

--- ms-api/src/test/java/ua/ms/services/UserServiceTest.java
+++ ms-api/src/test/java/ua/ms/services/UserServiceTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.NonTransientDataAccessException;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
@@ -20,6 +22,7 @@ import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.doNothing;
@@ -48,7 +51,7 @@ class UserServiceTest {
     @Test
     @DisplayName("test throwing exception for duplicates while registering")
     void shouldThrowDuplicate() {
-        when(userRepository.findByUsername(anyString())).thenReturn(Optional.of(USER_ENTITY));
+        when(userRepository.save(USER_ENTITY_FOR_REGISTRATION)).thenThrow(DataIntegrityViolationException.class);
         assertThatThrownBy(() -> userService.register(USER_CREDENTIALS))
                 .isInstanceOf(RuntimeException.class)
                 .isInstanceOf(EntityDuplicateException.class);
