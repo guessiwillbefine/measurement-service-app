a5cab9db26385be2afaa3e524fcc7024f638ff99
DeepPurpleTest <110846449+DeepPurpleTest@users.noreply.github.com>
2023-05-11 01:28:35 +0300

    Some machine changes (#36)

--- ms-api/src/main/java/ua/ms/entity/machine/dto/view/MachineView.java
+++ ms-api/src/main/java/ua/ms/entity/machine/dto/view/MachineView.java
@@ -3,7 +3,6 @@ package ua.ms.entity.machine.dto.view;
 import ua.ms.entity.machine.AbstractMachineIdentifiable;
 import ua.ms.entity.machine.MachineActivity;
 import ua.ms.entity.machine.MachineType;
-import ua.ms.entity.sensor.Sensor;
 import ua.ms.entity.sensor.dto.view.SensorView;
 
 import java.util.List;
--- ms-api/src/main/java/ua/ms/service/MachineService.java
+++ ms-api/src/main/java/ua/ms/service/MachineService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ua.ms.entity.machine.AbstractMachineIdentifiable;
 import ua.ms.entity.machine.Machine;
+import ua.ms.entity.machine.MachineActivity;
 import ua.ms.entity.machine.dto.MachineDto;
 import ua.ms.service.repository.MachineRepository;
 import ua.ms.util.exception.EntityNotFoundException;
@@ -35,6 +36,7 @@ public class MachineService {
 
     @Transactional
     public Machine save(final Machine machine) {
+        machine.setActivity(MachineActivity.INACTIVE);
         return machineRepository.save(machine);
     }
 
